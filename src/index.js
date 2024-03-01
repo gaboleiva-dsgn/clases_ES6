5b243c2bf965620e86fd62b87f16982c718ad310
Alicia Vento <aliciavento@gmail.com>
2024-03-01 20:06:31 -0300

    Validacion

--- src/index.js
+++ src/index.js
@@ -2,23 +2,18 @@
 import Cliente from "./cliente.js";
 import Impuesto from "./impuestos.js";
 
-let impuesto = new Impuesto (5000, -222220);
+let impuesto = new Impuesto(5000, -220);
 let cliente = new Cliente("Alicita", impuesto);
 
-validacion();
+// Llama a validacion pasando los valores de montoBrutoAnual y deducciones como argumentos
+validacion(cliente._impuesto.montoBrutoAnual, cliente._impuesto.deducciones);
 
-function validacion() {
-    if (typeof montoBrutoAnual !== 'number' || montoBrutoAnual < 0) {
-        console.log('montoBrutoAnual debe ser un número positivo');
-    } else if (typeof deducciones !== 'number' || deducciones < 0) {
-            console.log('deducciones debe ser un número positivo');
-    }
-    else {
-        console.log(cliente.calcularImpuestos());
-    }
-};
-
-
-
-
-
+function validacion(montoBrutoAnual, deducciones) {
+  if (typeof montoBrutoAnual !== "number" || montoBrutoAnual < 0) {
+    console.log("montoBrutoAnual debe ser un número positivo");
+  } else if (typeof deducciones !== "number" || deducciones < 0) {
+    console.log("deducciones debe ser un número positivo");
+  } else {
+    console.log(cliente.calcularImpuestos());
+  }
+}
