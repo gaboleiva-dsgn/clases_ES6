eb46d239c10281570ec834fafdf7b4581fec15c8
Gabriel Leiva <g.leivach@gmail.com>
2024-03-01 17:50:51 -0300

    Cambio en validación al index

--- src/impuestos.js
+++ src/impuestos.js
@@ -8,23 +8,13 @@ class Impuesto{
         return this._montoBrutoAnual;
     }
     set montoBrutoAnual(montoBrutoAnual){
-        if (typeof montoBrutoAnual !== 'number' || montoBrutoAnual < 0) {
-            console.log('montoBrutoAnual debe ser un número positivo');
-        }
-        this._montoBrutoAnual = () => {
-            return montoBrutoAnual;
-        }
+        this._montoBrutoAnual = montoBrutoAnual;
     }
     get deducciones(){
         return this._deducciones;
     }
     set deducciones(deducciones){
-        if (typeof deducciones !== 'number' || deducciones < 0) {
-            console.log('deducciones debe ser un número positivo')
-        }
-        this._deducciones = () => {
-            return deducciones;
-        } 
+        this._deducciones = deducciones;
     }
 };
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@
 import Cliente from "./cliente.js";
 import Impuesto from "./impuestos.js";
 
-let impuesto = new Impuesto (5000, 222220);
+let impuesto = new Impuesto (5000, -222220);
 let cliente = new Cliente("Alicita", impuesto);
-console.log(cliente.calcularImpuestos());
+
+validacion();
+
+function validacion() {
+    if (typeof montoBrutoAnual !== 'number' || montoBrutoAnual < 0) {
+        console.log('montoBrutoAnual debe ser un número positivo');
+    } else if (typeof deducciones !== 'number' || deducciones < 0) {
+            console.log('deducciones debe ser un número positivo');
+    }
+    else {
+        console.log(cliente.calcularImpuestos());
+    }
+};
+
+
 
 
 
